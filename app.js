b1619b0eed8ce14c0649345c243a8aa31eae51b3
justpramod <adhikaripramod864@gmail.com>
2026-08-03 19:32:05 +0545

    starts server after DB connects, add central error handler

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ app.use(express.json());
 const taskRouter = require('./routes/tasks');
 const authRouter = require('./routes/auth');
 const protect = require('./middleware/protect');
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log('MongoDB connected!!'))
-.catch((err)=> console.log('Connection error: ', err));
 
 app.use('/auth',authRouter);
+app.use('/tasks', protect, taskRouter);
 
+app.use((req,res)=>{
+    res.status(404).json({message: 'Route not found!'});
+});
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).json({message: 'server error'});
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> {
+    console.log('MongoDB connected!!');
+    app.listen(process.env.PORT || 3000, ()=> console.log('Server on http://localhost:3000'));
+})
+.catch((err)=> 
+    {
+        console.log('Connection error: ', err);
+        process.exit(1);
+    });
